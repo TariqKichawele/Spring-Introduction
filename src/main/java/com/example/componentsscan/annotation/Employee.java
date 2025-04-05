ec624c515ce3ea66ffd0f3a7eddaad496f267f24
TariqKichawele <tariqkichawele66@gmail.com>
2025-04-05 10:52:45 -0400

    Add initial implementation of employee application with component scanning
    - Created App and Employee classes for the application using XML and annotation-based configuration.
    - Added component scanning configuration in componentScanDemo.xml and AppConfig.java.
    - Implemented Employee class with properties and methods for employee details.
    - Included XML configuration for Spring to define beans for Employee.

---
+++ src/main/java/com/example/componentsscan/annotation/Employee.java
@@ -0,0 +1,51 @@
+package com.example.componentsscan.annotation;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Employee {
+    private int employeeId;
+    @Value("Hello")
+    private String firstName;
+    private String lastName;
+    private double salary;
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee [employeeId=" + employeeId + ", firstName=" + firstName + ", lastName=" + lastName
+                + ", salary=" + salary + "]";
+    }
+}
